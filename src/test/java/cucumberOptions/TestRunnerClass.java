585f13c0c3c68765da3d2796f458d418bd9a292e
yogi0067 <yogesh.bansa0067@gmail.com>
2020-02-27 04:11:02 +0530

    Basic Framework with HTML Reporting.

--- src/test/java/cucumberOptions/TestRunnerClass.java
+++ src/test/java/cucumberOptions/TestRunnerClass.java
@@ -8,7 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/features",
-		glue = {"stepDefinition"}
+		glue = {"stepDefinition"},
+		plugin = "json:target/cucumner-json-reports/cucumber-json-report.json"
+		
 		)
 public class TestRunnerClass {
 
